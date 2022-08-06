1ace9fbee0c01633c9655b6f42d946fe6a4c61b9
DmytroZn <dmytro.zn@gmail.com>
2022-08-06 15:03:19 +0300

    update structure db

--- track_finance/tracking/models.py
+++ track_finance/tracking/models.py
@@ -2,7 +2,8 @@ from decimal import Decimal
 
 from django.contrib.auth.models import PermissionsMixin, UserManager, AbstractBaseUser
 from django.db import models
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, RegexValidator
+
 
 # Create your models here.
 
@@ -37,9 +38,11 @@ class Spending(models.Model):
 
 class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=20, unique=True)
-    first_name = models.CharField(max_length=20, unique=True)
-    last_name = models.CharField(max_length=20, unique=True)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
     email = models.EmailField()
+    phoneNumberRegex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
+    phoneNumber = models.CharField(validators=[phoneNumberRegex], max_length=16, unique=True, blank=True)
     is_staff = models.BooleanField()
     objects = UserManager()
 
